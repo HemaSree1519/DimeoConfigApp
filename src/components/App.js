bdaf18b0c97c0b7fb5a5c3ed076eece57a10d56c
hemasree.kotari <hemasree.kotari@everest.engineering>
2019-12-18 16:58:25 +0530

    Seperate styles from component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {styles} from '../styles/App.styles'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Dimeo Config Application</Text>
+    </View>
+  );
+}
+
